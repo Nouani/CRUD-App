a424040aa0e679ba69efeeb718fd4da20b0d563f
Nouani <nouanigabriel@hotmail.com>
2020-01-27 19:00:50 -0300

    Rotas de cadastro, listagem e alteração de dados

--- index.js
+++ index.js
@@ -3,8 +3,37 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' });
-})
+let projects = [];
+
+app.post('/projects', (req, res) => {
+    const { id, title } = req.body;
+    
+    projects.push({id, title, tasks: []});
+    
+    return res.json(projects);
+});
+
+app.get('/projects', (req, res) => {
+    return res.json(projects);
+});
+
+app.put('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    projects = projects.filter(project => {
+        if (project.id == id){
+            project.title = title;
+        }
+
+        return project;
+    });
+
+    return res.json(projects);
+});
+
+app.delete('/projects/:id', (req, res) => {
+    
+});
 
 app.listen(3333);
